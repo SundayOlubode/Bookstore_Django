bf6242e0efcee08f5a37fbb858a371aeb6e83cd3
Samuel <molubode81@gmail.com>
2023-05-19 17:36:27 +0100

    Add some changes

--- api/views.py
+++ api/views.py
@@ -1,7 +1,5 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import obtain_auth_token
 
 import json
 
